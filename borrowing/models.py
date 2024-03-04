dc932cd15a5a8c360af5543c15c3d5a293094076
Ivan <mozharivan17@gmail.com>
2024-03-04 10:07:11 +0200

    Create borrow model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrow(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
