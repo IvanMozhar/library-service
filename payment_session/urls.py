620e2d8bac568323aa46e78bd39e9e5b52840d27
Ivan <mozharivan17@gmail.com>
2024-03-12 11:53:35 +0200

    Add paths to payment_session urls

--- payment_session/urls.py
+++ payment_session/urls.py
@@ -4,19 +4,15 @@ from rest_framework.routers import DefaultRouter
 
 from payment_session.views import PaymentViewSet, create_checkout_session
 
-# from payment_session.views import create_payment, success, cancel
+from payment_session.views import success, cancel
 
-# urlpatterns = [
-#     path("create-payment/", create_payment, name="create_payment"),
-#     path("success/", success, name="payment_success"),
-#     path("cancel/", cancel, name="payment_cancel"),
-# ]
 router = routers.DefaultRouter()
 router.register("payments", PaymentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("create-checkout-session/", create_checkout_session, name="create-checkout-session")
+    path("success/", success, name="success"),
+    path("cancel/", cancel, name="cancel"),
 ]
 
 app_name = "payment_session"
