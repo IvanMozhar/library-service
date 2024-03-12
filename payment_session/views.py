0fd6de44f3c4f92b661e30171d57c113f7a97e2a
Ivan <mozharivan17@gmail.com>
2024-03-12 11:53:14 +0200

    Create success and canceled paths

--- payment_session/views.py
+++ payment_session/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from library_service import settings
 from .models import Payment
-from payment_session.stripe import create_stripe_session
 from rest_framework.decorators import api_view
 
 from .serializers import PaymentSerializer
@@ -22,67 +21,32 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def create_checkout_session(request, borrowing):
+    price = borrowing.calculate_amount
     session = stripe.checkout.Session.create(
         line_items=[{
             "price_data": {
                 "currency": "usd",
                 "product_data": {
-                    "name": "Coffee",
+                    "name": borrowing.book_id.title,
                 },
-                "unit_amount": 1,
+                "unit_amount": int(price * 100),
             },
             "quantity": 1,
         }],
         mode="payment",
-        success_url="http://127.0.0.1:8000",
-        cancel_url="http://127.0.0.1:8000",
-    )
-    payment = Payment.objects.create(
-        status="PENDING",
-        type="PAYMENT",
-        session_url=session.url,
-        session_id=session.id,
-        borrowing=borrowing,
-        money_to_pay=amount,
-        user=request.user
+        success_url="http://127.0.0.1:8000/api/payments/success/",
+        cancel_url="http://127.0.0.1:8000/api/payments/cancel/",
     )
 
-    return redirect(session.url, code=303)
+    return session
 
-# @api_view(["POST"])
-# def create_payment(request):
-#     stripe.api_key = settings.STRIPE_SECRET_KEY
-#     checkout_session = stripe.checkout.Session.create(
-#         payment_method_types=["card"],
-#         line_items=[
-#             {
-#                 "price_data": {
-#                     "currency": "usd",
-#                     "unit_amount": 2000,
-#                 },
-#                 "quantity": 1,
-#             }
-#         ],
-#         mode="payment",
-#         success_url="http://127.0.0.1:8000/success",
-#         cancel_url="http://127.0.0.1:8000/cancel",
-#     )
-#
-#     return redirect(checkout_session.url, code=303)
-#
-#
-# @api_view(["GET"])
-# def success(request):
-#     stripe.api_key = settings.STRIPE_SECRET_KEY
-#     session_id = request.GET.get("session_id")
-#     session = stripe.checkout.Session.retrieve(session_id)
-#     customer = stripe.Customer.retrieve(session.customer)
-#     user_id = request.user.id
-#     user_payment = Payment.objects.get(user_id=user_id)
-#     user_payment.stripe_url = session_id
-#     return JsonResponse({"message": "Payment succeded"})
-#
-#
-# @api_view(["GET"])
-# def cancel(request):
-#     return JsonResponse({"message": "Payment cancelled or failed"})
+
+@api_view(["GET"])
+def success(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    return JsonResponse({"message": "Payment succeded"})
+
+
+@api_view(["GET"])
+def cancel(request):
+    return JsonResponse({"message": "Payment cancelled or failed"})
