c6896c91c810f4da470fb0a6c7ce2980497c7946
Ivan <mozharivan17@gmail.com>
2024-03-12 10:26:50 +0200

    Add calculate amount method

--- borrowing/models.py
+++ borrowing/models.py
@@ -10,3 +10,15 @@ class Borrow(models.Model):
     actual_return_date = models.DateField(null=True, blank=True)
     book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    @property
+    def calculate_amount(self):
+        dif = self.expected_return_date - self.borrow_date
+        if 20 > dif.days >= 10:
+            return self.book_id.daily_fee * 1.3
+        elif 30 > dif.days >= 20:
+            return self.book_id.daily_fee * 1.4
+        elif dif.days >= 30:
+            return self.book_id.daily_fee * 1.7
+        else:
+            return self.book_id.daily_fee
