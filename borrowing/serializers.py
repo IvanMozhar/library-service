020c29a352b09e75350eb281397f99667150f025
Ivan <mozharivan17@gmail.com>
2024-03-13 06:03:01 +0200

    Add payment_detail to serializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,15 +1,16 @@
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from books.models import Book
 from books.serializers import BookSerializer
 from borrowing.models import Borrow
+from payment_session.models import Payment
+from payment_session.serializers import PaymentBorrowSerializer
 
 
 class BorrowBookSerializer(serializers.ModelSerializer):
     book = BookSerializer(many=False, source="book_id")
     user = serializers.StringRelatedField(many=False, source="user_id")
+    payment_details = serializers.SerializerMethodField()
 
     class Meta:
         model = Borrow
@@ -20,8 +21,15 @@ class BorrowBookSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "book",
             "user",
+            "payment_details",
         )
         read_only_fields = ["user", "actual_return_date"]
+
+    def get_payment_details(self, obj):
+        payment = Payment.objects.get(borrowing=obj)
+        if payment:
+            return PaymentBorrowSerializer(payment).data
+        return None
 
 
 class BorrowCreateSerializer(serializers.ModelSerializer):
@@ -29,12 +37,7 @@ class BorrowCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Borrow
-        fields = [
-            "borrow_date",
-            "expected_return_date",
-            "book_id",
-            "user_id"
-        ]
+        fields = ["borrow_date", "expected_return_date", "book_id", "user_id"]
         read_only_fields = ["user_id"]
 
 
@@ -42,6 +45,4 @@ class ReturnBookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Borrow
-        fields = (
-            "actual_return_date",
-        )
+        fields = ("actual_return_date",)
